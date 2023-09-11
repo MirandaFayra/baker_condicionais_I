548d33dfe9387a1dd0d30a82e3fba00608d8aa4c
Fayra Miranda <52434685+MirandaFayra@users.noreply.github.com>
2023-09-11 15:38:46 -0300

    Finalizar a base das explicacoes

--- scripts.js
+++ scripts.js
@@ -1,4 +1,43 @@
 // -------------- RELEMBRANDO ----------------
+/*
+- OPERADORES RELACIONAIS => 
+
+    === Verifica se o valor e tipo são iguais 
+
+    !== Verifica se o valor e tipo são diferente
+
+    > Primeiro, maior que o segundo 
+
+    < Primeiro é menor que o segundo 
+
+    >= Verifica se o primeiro é maior ou igual ao segundo 
+
+    <= Verifica se o primeiro é menor ou igual ao segundo 
+
+Importante: Tomar cuidado com a sequencia dos símbolos, para não confundir com arrown function 
+
+----
+
+- OPERADORES LÓGICOS : 
+
+    && (E) => Verifica se ambas condicoes são verdadeiras 
+
+    || (OU)=> Verifica se pelo menos uma condição é verdadeira 
+
+    ! (NÃO)=> Oposto da resposta 
+
+-----
+
+- TABELA VERDADE => 
+
+- Quando é o operador E , só será verdade se os dois termos forem verdadeiros 
+
+- Quando é o operador OU, só será falso se os dois termos forem falsos.
+
+- Comparação retorna um valor boleano 
+
+*/
+
 //-------------- EXERICICIO 1 ----------------
 //-------------- EXERICICIO 2 ----------------
 //-------------- EXERICICIO 3 ----------------
